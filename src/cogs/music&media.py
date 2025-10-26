1e7b0693f718067814338a66b1a853ff26c7b69d
Happy Harsh <81058137+happyharsh-codes@users.noreply.github.com>
2025-10-26 10:46:32 +0530

    Update music&media.py

--- src/cogs/music&media.py
+++ src/cogs/music&media.py
@@ -1,16 +1,93 @@
 from __init__ import*
+import yt_dlp
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
 
-class MuiskMedia(commands.Cog):
+class Muisk_and_Media(commands.Cog):
     def __init__(self, client: commands.Bot):
         self.client = client
+        self.sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(
+            client_id="SPOTIFY_CLIENT_ID",
+            client_secret="SPOTIFY_CLIENT_SECRET"
+        ))
+        
+    async def music_player(self, ctx, action):
+        """Action can be pause, play, rewind, or skip"""
+        em = Embed(color= Color.green())
+        channel = 0
+        player = []
+        for i in MUSIC:
+            if str(ctx.guild.id) in i:
+                channel = i.split("_")[1]
+                player = MUSIC[i]
+        if action == "play":
+            em.add_author(name= player[0]["name"], icon_url = player[0]["emoji"])
+            em.title = f"▶️ Now Playing"
+            em.description = f"[**{player[0]["name"]}**]({player[0]["link"]})"
+            em.thumbnail(url= player[0]["thumbnail_url"])
+        elif action == "stop":
+            em.add_author(name= player[0]["name"], icon_url= player[0]["emoji"])
+            em.title = "⏸️ Stopped Playing"
+            em.description = f"Last playing: [**{player[0]["name"]}**]({player[0]["link"]})"
+        elif action == "rewind":
+            em.add_author(name= player[0]["name"], icon_url= player[0]["emoji"])
+            em.title = f"⏮️ Song Rewinded"
+            em.description = f"[**{player[0]["name"]}**]({player[0]["link"]})"
+            em.thumbnail(url= player[0]["thumbnail_url"])
+            em.set_footer(f"Rewinded by {ctx.author.display_name}", icon_url= ctx.author.avatar)
+        elif action == "skip":
+            pass
+        await ctx.send(embed=em)
 
+    def search_spotify(self, track):
+        results = self.sp.search(track,limit=1, type= "track")
+        track = {"name": "", "link": "", "thumbnail_url": "", "emoji": "spotify", "audio": ""}
+        tracks = results["track"]["items"][0]:
+        if tracks:
+            track["name"] = tracks["name"]
+            track["link"] = tracks["external_urls"]["spotify"]
+            track["thumbnail_url"] = tracks["album"]["images"][0]["url"]
+            
+        else:
+            return self.search_yt(track)
+    def search_yt(self, track):
+        track = {"name": "", "link": "", "thumbnail_url": "", "emoji": "spotify"}
+        
+        pass
+    def search_songcloud(self, track):
+        track = {"name": "", "link": "", "thumbnail_url": "", "emoji": "spotify"}
+        
+        pass
+    
     @commands.command(aliases=["p"])
     @commands.cooldown(1,100, type = commands.BucketType.user )
     @commands.has_permissions()
     @commands.bot_has_permissions()
     async def play(self, ctx, search):
         """Plays the Song music 🎶 on your VC"""
-        await ctx.send("This command is yet to be made :/")
+        #Joining Vc
+        if not ctx.author.voice or not ctx.author.voice.channel:
+            await ctx.reply("You must be in a voice channel to run this command", delete_after= 12)
+            return
+        channel = ctx.author.voice.channel
+        #checking perms
+        permissions = channel.permissions_for(ctx.guild.me)
+        if not permissions.connect:
+            await ctx.reply("I do not have permissions to join this channel. Please try again in another channel.", delete_after = 12)
+            return
+        if not permissions.speak:
+            await ctx.reply("I do not have permissions to speak in this channel. Please join another channel and try again", delete_after = 12)
+            return
+        voice_client = ctx.guild.voice_client
+        if voice_client:
+            await voice_client.move_to(channel)
+        else:
+            voice_client = await channel.connect()
+            for i in MUSIC:
+                if str(ctx.guild.id) in i:
+                    MUSIC.pop(i)
+                    break
+        
 
     @commands.command(aliases=["q"])
     @commands.cooldown(1,100, type = commands.BucketType.user )
@@ -37,5 +114,5 @@ class MuiskMedia(commands.Cog):
         pass
         
 async def setup(bot):
-    await bot.add_cog(MuiskMedia(bot))
+    await bot.add_cog(Muisk_and_Media(bot))
     print("Loaded cogs: MusikMedia")
