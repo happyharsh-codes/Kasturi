79ec06a6d2ab31fa2d7c23ffdb78107e49ce580a
Happy Harsh <81058137+happyharsh-codes@users.noreply.github.com>
2025-10-26 07:33:52 +0530

    Update dev_tech_tools.py

--- src/cogs/dev_tech_tools.py
+++ src/cogs/dev_tech_tools.py
@@ -1,4 +1,5 @@
 from __init__ import*
+import requests
 
 class Dev_Tech_Tools(commands.Cog):
     def __init__(self, client: commands.Bot):
@@ -48,13 +49,72 @@ class Dev_Tech_Tools(commands.Cog):
     @commands.has_permissions()
     @commands.bot_has_permissions()
     async def yt(self, ctx, *, search: str):
-        em = Embed(
-            title="🔍 YouTube Search",
-            description=f"Results for `{search}` will be here soon!",
-            color=Color.red()
-        )
-        em.set_footer(text=f"Requested by {ctx.author}", icon_url=ctx.author.avatar)
-        await ctx.send(embed=em)
+        search_query = search.replace(" ", "+")
+        url = (
+            "https://www.googleapis.com/youtube/v3/search?"
+            "part=snippet&"
+            f"maxResults=10&q={search_query}&"
+            "type=video&"
+            "key=AIzaSyA95DL_DoZMbLZ2brkhWMrfNUq-ErJXYuA"
+            )
+        videos = []
+        page = 1
+        response = requests.get(url).json()
+        for item in response.get("items", []):
+            if item["id"]["kind"] == "youtube#video":
+                video_title = item["snippet"]["title"]
+                video_id = item["id"]["videoId"]
+                video_link = f"https://www.youtube.com/watch?v={video_id}"
+                thumbnail_url = item["snippet"]["thumbnails"]["default"]["url"]
+                author_name = item["snippet"]["channelTitle"]
+                channel_id = item["snippet"]["channelId"]
+                url2 = f'https://www.googleapis.com/youtube/v3/channels?part=snippet&id={channel_id}&key=AIzaSyA95DL_DoZMbLZ2brkhWMrfNUq-ErJXYuA'
+                response2 = requests.get(url2).json()
+                channel_avatar = response2['items'][0]['snippet']['thumbnails']['default']['url']
+                
+                vid_info = {"title": video_title, "link": video_link, "thumbnail_url": thumbnail_url, "author":author_name, "avatar_url": channel_avatar}
+                videos.append(vid_info)
+
+        length = len(videos)
+        em = Embed(color=Color.dark_gold())
+        if not videos:
+            return await ctx.reply("No Video found for the search")
+        def updator():
+            nonlocal em, videos, page, watch
+            current_vid = videos[page-1]
+            em.title = f"**{current_vid["title"]}**"
+            em.url = current_vid["link"]
+            em.set_image(url= current_vid["thumbnail_url"])
+            em.set_author(name= current_vid["author"], icon_url= current_vid["avatar_url"])
+            em.set_footer(text=f"Showing video {page} out of {length} | {ctx.author.id}", icon_url = ctx.author.avatar)
+            watch.url = current_vid["link"]
+        updator()
+        left = Button(style=ButtonStyle.secondary, custom_id= "left", disabled=True, row=0, emoji=discord.PartialEmoji.from_str("<:leftarrow:1427527800533024839>"))
+        right = Button(style=ButtonStyle.secondary, custom_id= "right", row=0, emoji=discord.PartialEmoji.from_str("<:rightarrow:1427527709403119646>"))
+        watch = Button(style=ButtonStyle.link, custom_id = "watch", row=0, label = "Watch")
+        view = View(timeout= 45)
+        view.add_item(left)
+        view.add_item(watch)
+        view.add_item(right)
+
+        async def on_timeout():
+            nonlocal embed, msg
+            em.color = Color.greyple()
+            await msg.edit(embed = em, view = None)
+        view.on_timeout = on_timeout
+        async def on_leftright(interaction: Interaction):
+            nonlocal left, right, page, length, updator, em, view
+            if interaction.data["custom_id"] == "left":
+                page -= 1
+            else: page += 1
+            left.disabled = (page == 1)
+            right.disabled = (page == length)
+            updator()
+            await interaction.response.edit_message(embed = em, view = view)
+            
+        left.callback = on_leftright
+        right.callback = on_leftright
+        msg = await ctx.reply(embed= em, view= view)
 
     @commands.command(aliases=["codesnippet"])
     @commands.cooldown(1, 60, type=commands.BucketType.user)
