3a28c9026feca82a87dfb4a67ee38155f96e2edf
Happy Harsh <81058137+happyharsh-codes@users.noreply.github.com>
2025-10-31 20:38:37 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -167,7 +167,7 @@ class ReportBugModal(discord.ui.Modal):
         button.disabled = True
         await msg.edit(view=view)
         await interaction.channel.send(embed= Embed(title= "Your Response has been recored successfully", color = Color.green()))
-        em = Embed(title = f"Bug Reported by {ctx.author.display_name}", description= f"**Username**: {ctx.author.name}\n**Id**: {ctx.author.id}\n**Guild**: [{ctx.guild.name}]({Server_Settings[str(ctx.guild.id)]["invite_link"]})\n**Report**: {self.input_box.value}", color = Color.green())
+        em = Embed(title = f"Bug Reported by {ctx.author.display_name}", description= f"**Username**: {ctx.author.name}\n**Id**: {ctx.author.id}\n**Guild**: [{ctx.guild.name}]({Server_Settings[str(ctx.guild.id)]['invite_link']})\n**Report**: {self.input_box.value}", color = Color.green())
         em.set_footer(text= f"Reported by {ctx.author.name} | {timestamp(ctx)}", icon_url = ctx.author.avatar)
         view = BugReportView()
         await interaction.client.get_user(894072003533877279).send(embed = em, view = view)
