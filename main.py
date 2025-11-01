3322bc0ac1198924355cfb5829581ae1e26c1700
Happy Harsh <81058137+happyharsh-codes@users.noreply.github.com>
2025-11-01 12:55:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -34,6 +34,7 @@ async def start():
     client.add_listener(bot.on_error)
     client.add_listener(bot.on_disconnect)
     client.add_listener(bot.on_presence_update)
+    client.add_listener(bot.on_member_update)
     client.after_invoke(bot.after_any_command)
     client.before_invoke(bot.before_any_command)
     try:
