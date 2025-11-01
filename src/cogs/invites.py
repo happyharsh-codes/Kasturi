e556b226ef202101363b92b684dafed8ee4aa3f3
Happy Harsh <81058137+happyharsh-codes@users.noreply.github.com>
2025-11-01 07:35:18 +0530

    Create invites.py

---
+++ src/cogs/invites.py
@@ -0,0 +1,35 @@
+from __init__ import *
+
+class Invites_Tracker(commands.Cog):
+    def __init__(self, client: commands.Bot):
+        self.client = client
+
+    @commands.command(aliases=["kelly_mute", "kmute"])
+    @commands.cooldown(1, 10, type=commands.BucketType.user)
+    @commands.has_permissions(manage_roles=True)
+    @commands.bot_has_permissions(manage_roles=True)
+    async def invites(self, ctx: commands.Context, member: discord.Member, *, reason: str = "No reason provided"):
+        """Shows how many users have Joines using your invite link."""
+        server = INVITER.get(str(ctx.guild.id), None)
+        if not server:
+            await ctx.send(embed = Embed(title= "No Record Found for this Guild"))
+        invited = []
+        for invited, inviter in server.items():
+            if inviter == ctx.author.id:
+                invited.append(int(invited))
+        em = Embed(title="Showing Invites Profile", description= f"**Total People Invited**: {len(invited)}\n", color = Color.purple())
+        if invited:
+            if len(invited) > 5:
+                invited = invited[:5]
+            for id in invited:
+                user = self.client.get_user(int(id))
+                if user:
+                    em.description += f"{user.mention}\n"
+                else:
+                    em.description += f"@unknown"
+        em.set_footer(text=f"Requested by {ctx.author.name} at {timestamp(ctx)", icon_url= ctx.author.avatar)
+        await ctx.send(embed = em)
+
+async def setup(bot):
+    await bot.add_cog(Moderation(bot))
+    print("Loaded cogs: Moderation")
