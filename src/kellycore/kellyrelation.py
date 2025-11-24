ab08f07a0f6448b4ed95c9379a54a1479d6fad84
Happy Harsh <81058137+happyharsh-codes@users.noreply.github.com>
2025-11-24 07:20:52 +0530

    Update kellyrelation.py

--- src/kellycore/kellyrelation.py
+++ src/kellycore/kellyrelation.py
@@ -18,17 +18,79 @@ class KellyRealtion:
             return None
         return Relation[str(id)]
     
-    def modifyUserRespect(self, val, id):
-        if str(id) not in Relation:
-            Relation[str(id)] = val
-        Relation[str(id)] += val
-        if Relation[str(id)] > 80: # Think of making Friend
-            pass
-        if Relation[str(id)] < -20: # Think of ban
-            pass
-        if Relation[str(id)] == 0:
-            Relation[str(id)] += 1
+    import random
+
+    async def modifyUserRespect(self, val, user_id):
+       uid = str(user_id)
+
+        if uid not in Relation:
+            Relation[uid] = val
+        else:
+            Relation[uid] += val
+
+        # Avoid 0 neutral dead zone
+        if Relation[uid] == 0:
+            Relation[uid] += 1
+
+        respect = Relation[uid]
+
+        # --- FRIEND LOGIC ---
+        if respect > 80:
+            await self.thinkFriendAction(user_id)
+
+        # --- BAN LOGIC ---
+        if respect < -20:
+            await self.thinkBanAction(user_id)
+
         return
+
+    async def thinkFriendAction(self, user_id):
+        """Bot thinks of making user a friend when respect is high."""
+        user = self.client.get_user(int(user_id))
+        if not user:
+            return
+
+        # Think message
+        if user_id in Database["friends"]:
+            return
+        # Action chance
+        roll = random.randint(1, 100)
+        # 60% think only
+        if roll <= 60:
+            await user.send("I think we can be great friends someday 😊")
+        # 25% make friend
+        elif roll <= 85:
+            await user.send("🎉 I have decided! You are now my friend!")
+            Database["friends"].append(user_id)
+        # 15% small reward
+        else:
+            await user.send("✨ You're special! Here's a small gift for being nice. Join official Server to recieve your reward")
+            # Give reward logic here
+
+    async def thinkBanAction(self, user_id):
+        """Bot thinks of punishing the user when respect is low."""
+        user = self.client.get_user(int(user_id))
+        if not user:
+            return
+
+        # Think message
+        await user.send("😠 I’m thinking… You’re being a bit rude to me.")
+
+        roll = random.randint(1, 100)
+
+        # 60% think only
+        if roll <= 60:
+            await user.send("I'll ignore this for now.")
+        # 25% mute instead of ban
+        elif roll <= 85:
+            await user.send("🔇 I’m muting you for a bit...")
+            # Mute
+            await self.muteUser(user)
+        # 15% actual ban
+        else:
+            await user.send("⛔ You crossed the line. Goodbye!")
+            # Ban 
+            await self.banUser(user)
     
     def addUserInfo(self, info, userid):
         if str(userid) in Behaviours:
