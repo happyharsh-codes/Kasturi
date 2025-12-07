e77fe45b7c4dcdbdf4de0b80eb72c3de98de49d1
Happy Harsh <81058137+happyharsh-codes@users.noreply.github.com>
2025-12-07 16:14:50 +0530

    Update game_functions.py

--- functions/game_functions.py
+++ functions/game_functions.py
@@ -6,6 +6,7 @@ from __init__ import*
 '''
 default_profiles = {
     "name": "No name noob",
+    "activity": "sleeping",
     "health": 100,
     "hunger": 100,
     "location": "home",
@@ -24,7 +25,9 @@ default_profiles = {
     "vehicles": {},
     "quests": {},
     "places": {},
-    "jobs": {}
+    "jobs": {},
+    "tasks": {},
+    "reminders": {}
 }
 '''
 
@@ -45,6 +48,54 @@ def weighted_choice(choices: list):
             return item
         upto += w
     return choices[-1][0]
+
+
+async def perform_task(task, uid, client):
+    
+    if task["name"] == "studying":
+        em = Embed(title="Currently Studying", description= f"Ayoo user you are currently studying", color = Color.orange())
+        em.set_footer(text = f"{task['name'].title()} - {remaining//task['duration']}% Completed")
+        channel = client.get_channel(task["channel"])
+    
+    if not channel:
+        try:
+            channel = await client.fetch_channel(task["channel"])
+        except:
+            return -1
+    await channel.send(embed= em, view= view)
+
+async def perform_reminder(reminder, uid, client):
+    
+    if task["name"] == "studying":
+        em = Embed(title="Currently Studying", description= f"Ayoo user you are currently studying", color = Color.orange())
+        em.set_footer(text = f"{task['name'].title()} - {remaining//task['duration']}% Completed")
+        channel = client.get_channel(task["channel"])
+    
+    if not channel:
+        try:
+            channel = await client.fetch_channel(task["channel"])
+        except:
+            return -1
+    await channel.send(embed= em, view= view)
+    
+async def run_all_tasks(client):
+    """Runs all the task from all users that hit the time limit
+    includes task like - travel, working, studying, crafting, exploring, mining, mob spawn."""
+    for id, profile in Profiles.items():
+        if profile["tasks"]:
+            for due, task in profile["tasks"]:
+                if datetime.now() > datetime.fromisoformat(due):
+                    await perform_task(task, id, client)
+
+async def run_all_reminders(client):
+    """Runs all reminders from all user that hit the limit. Reminders are usually for very long tasks.
+    Reminders includes - quests, marriage wishes, tips, build, etc"""
+    for id, profile in Profiles.items():
+        if profile["reminders"]:
+            for due, reminder in profile["reminders"]:
+                if datetime.now() > datetime.fromisoformat(due):
+                    await perform_reminder(reminder, id, client)
+
     
 def inv_searcher(id, item, amt):
     category= ["foods", "tools", "assets", "plants", "animals", "vehicles", "weapons", "emotes"]
@@ -78,7 +129,7 @@ def has_profile():
         except asyncio.TimeoutError:
             return False 
         if msg.content.lower() == code:
-            Profiles[str(ctx.author.id)] = {"name": ctx.author.name, "health": 100,"hunger": 100, "location": "home", "aura":0, "skills": {}, "foods": {}, "plants": {}, "animals": {}, "assets": {"cash": 100,"gem": 50,"orbs": 1}, "tools": {}, "weapons": {}, "vehicles": {}, "emotes": {}, "quests": {}, "places": {}, "jobs": {}}
+            Profiles[str(ctx.author.id)] = {"name": ctx.author.name, "activity": "sleeping", "health": 100,"hunger": 100, "location": "home", "aura":0, "skills": {}, "foods": {}, "plants": {}, "animals": {}, "assets": {"cash": 100,"gem": 50,"orbs": 1}, "tools": {}, "weapons": {}, "vehicles": {}, "emotes": {}, "quests": {}, "places": {}, "jobs": {}, "tasks": {}, "reminders": {}}
             em = Embed(title="Profile Created Successfully", description=f"{ctx.author.mention} your profile created successfully. Start playing eith game commands now: `hunt`, `chop`, `adv`, `mine`, `work`, `school`, `craft`, `use`, `eat` .., .\n:white_check_mark: You obatained bonous ₹100 cash 💵\n:white_check_mark: You obtained 50 gem 💎 and 1 Dark Magic Orb 🔮\nUse `k help games` to get more help and info.",color=Color.green())
             em.set_footer(text=f"{ctx.author.name} created acc at {timestamp(ctx)}", icon_url= ctx.author.avatar)
             await ctx.send(embed = em)
@@ -86,6 +137,64 @@ def has_profile():
             emoji = EMOJI[f"kelly{choice(['annoyed', 'laugh', 'gigle', 'waiting', 'idontcare', 'chips', 'bweh', 'bweh'])}"]
             await msg.reply(f"**{emoji} | ** you dont even do a single thing properly disgusting!! Dont ever come to me again")
         
+        return False
+    return commands.check(predicate)
+
+def not_busy()
+    async def predicate(ctx):
+        uid = str(ctx.author.id)
+        activity = Profiles[uid]["activity"]
+        tasks = Profiles[uid]["tasks"]
+        for due, t in tasks.items():
+            if t["name"] == activity:
+                task = t
+                duration = due
+                break
+        else:
+            return True
+        remaining = datetime.now() - datetime.fromisoformat(duration)
+        remaining_seconds = remaining.total_seconds()
+        minutes, seconds = divmod(remaining_seconds, 60)
+        percentage_completed = (remaining_seconds//task["duration"])*100
+        if activity == "sleeping":
+            return True 
+        elif activity == "working":
+            em = Embed(title="Currently Busy Working", description= f"Ayoo user you are currently **Working**. You cant perform that command now. Wait until your office hour ends to claim the Rewards.\n**Time Remaining**: {minutes:.0f}:{seconds:02.0f}", color = Color.orange())
+        elif activity == "studying":
+            em = Embed(title="Currently Studying", description= f"Ayoo user you are currently **Studying**. You cant perform that command now. Wait until you finish classes to claim the Rewards.\n**Time Remaining**: {minutes:.0f}:{seconds:02.0f}", color = Color.orange())
+        elif activity == "travelling":
+            em = Embed(title="Currently Traveling", description= f"Ayoo user you are currently **Travelling**. You cant perform that command now. Wait until you reach the destination .\n**Time Remaining**: {minutes:.0f}:{seconds:02.0f}", color = Color.orange())
+        elif activity == "exploring":
+            em = Embed(title="Currently Out Exploring", description= f"Ayoo user you are currently **Exploring**. You cant perform that command now. Wait until you finish to claim the Rewards.\n**Time Remaining**: {minutes:.0f}:{seconds:02.0f}", color = Color.orange())
+        em.set_footer(text = f"{task['name'].title()} - {percentage_completed}% Completed")
+        
+        view = View(timeout=45)
+        async timeout():
+            nonlocal em, view, msg
+            em.color = Color.light_grey()
+            for children in view.children:
+                children.disabled = True
+            await msg.edit(embed=em, view=view)
+        view.on_timeout = timeout
+
+        cancel = Button(label="✖️ Cancel", custom_id = activity, style = ButtonStyle.secondary)
+        async def on_cancel(inter: Interaction):
+            if inter.user.id != ctx.author.id:
+                await inter.response.send_message(embed=Embed(description="This interaction is not for you", color=Color.red()),ephemeral=True)
+                return
+            nonlocal view, cancel 
+            cancel.disabled = True
+            cancel.label = "🚫 Cancelled"
+            view.timeout = None
+            for due, task in Profiles[uid]["tasks"].items():
+                if task["name"] == inter.data["custom_id"]:
+                    del Profiles[uid]["tasks"][due]
+                    Profiles[uid]["activity"] = "sleeping"
+                    Profiles[uid]["location"] = "home"
+            await inter.response.edit_message(embed=em, view= view)
+        cancel.callback = on_cancel
+        view.add_item(cancel)
+        msg = await ctx.reply(embed=em, view= view)
         return False
     return commands.check(predicate)
     
@@ -120,7 +229,8 @@ def skills_manager(id: str, skill, percentage):
     if Profile[id]["skills"][skill] > 100:
         Profile[id]["skills"][skill] = 100
     elif Profile[id]["skills"][skill] <= 0:
-        del Profile[id]["skills"][skill]
+        del Profile[id][
+        "skills"][skill]
     
 def skills_searcher(ctx, skill, percentage):
     skills = Profiles[str(id)]["skills"]
