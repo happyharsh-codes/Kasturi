b803bebe92492b186dd890116df4126571e9a9c7
Happy Harsh <81058137+happyharsh-codes@users.noreply.github.com>
2025-12-19 14:20:45 +0530

    Update and rename ayaka.py to ayasaka.py

---
+++ src/kellycore/ayasaka.py
@@ -0,0 +1,88 @@
+from __init__ import*
+from src.kellycore.kellybusy import KellyBusy
+
+class Ayasaka:
+    """
+    Ayasaka - Kelly's Personal Assistant.
+
+    Responsibilities:
+        - Manage Kelly's schedule
+        - Maintain availability calendar
+        - Inform users when Kelly is free or busy/lazy
+        - Automatically reschedule tasks when Kelly is too tired/sleepy/lazy/busy
+        - Remind Kelly about pending tasks (from KellyBusy)
+        - Defer or decline user requests if overloaded
+        - KellyBusy - schedules manager
+    
+    """
+
+    def __init__(self, kelly):
+        self.kelly = kelly
+        self.busy = KellyBusy(kelly.memory._memory["schedules"])
+    
+    async def ayakasend(self, channel, content, uid):
+        try:
+            webhook = await channel.create_webhook(name="Ayaka")
+            async with channel.typing():
+                if isinstance(content, Embed):
+                    await webhook.send(content= f"<@{uid}> ", embed=content, username="Ayaka", avatar_url=f"https://raw.githubusercontent.com/happyharsh-codes/Kasturi/refs/heads/main/assets/ayaka_{randint(1,3)}.png")
+                else:
+                    await webhook.send(content= f"<@{uid}> " + content, username="Ayaka", avatar_url=f"https://raw.githubusercontent.com/happyharsh-codes/Kasturi/refs/heads/main/assets/ayaka_{randint(1,3)}.png")
+            await webhook.delete()
+        except:
+            async with channel.typing():
+                if isinstance(content, Embed):
+                    await channel.send(f"**Ayaka**: <@{uid}> ", embed=content)
+                else:
+                    await channel.send(f"**Ayaka**: <@{uid}> " + content)
+
+    async def remindKellyPerformTask(self):
+        pass
+
+    async def ayakaQuery(self, message, mood, type):
+        """
+        Handles user requests BEFORE Kelly receives them.
+        Flow:
+            1. Check KellyBusy
+            2. Manage schedule
+        """
+        uid = message.author.id
+       
+        # If Kelly is lazy, assistant manages
+        if self.kelly.status == "lazy":
+            prompt = f"You are Ayaka, Kelly's Assistant. Kelly is cute discord mod bot with lively attitude and sass. Kelly is currently very lazy, grumpy. Generate small Response with emojis. Inform user about Kelly's state. If user ask for a task, inform that you have added task in Kelly's schedules. Chat with user, flirt or simp and drive user away as its your only chance to make a move if needed or stay unamused. If its your first chat tell them they can chat with you by typing your name 'Ayasaka'."
+            response = getResponse(f"{message.author.display_name}: {message.content}", prompt)
+            self.kelly.memory.addUserChat(message.content, response, message.author.id, reply_by="Ayaka")
+            await self.ayakasend(message.channel, self.akayaEmojify(response), message.author.id)
+            command = self.kelly.search_commands(message)
+            if command:
+                self.busy.addScedules(user_id=message.author.id, command_name=command[0], params=command[1], message_id=message.id, channel_id=message.channel.id, priority=1):
+            return False
+        
+        # If schedule is overloaded assistant handles
+        if self.kelly.status == "busy":
+            prompt = f"You are Akaya, Kelly's Assistant. Kelly is cute discord mod bot with lively attitude and saas. Kelly's schedules are overloaded already and shes very busy. Generate small Response with emojis. Inform user about Kelly's state. Inform that tasks cant be added as Kelly's schedules are full if user sends a task then only. Chat with user, flirt or simp and drive user away as its your only chance to make a move if needed. If its your first chat tell them they can chat with you by typing your name 'Ayasaka'"
+            response = getResponse(f"{message.author.display_name}: {message.content}", prompt)
+            self.kelly.memory.addUserChat(message.content, response, message.author.id, reply_by="Ayaka")
+            await self.ayakasend(message.channel, self.akayaEmojify(response), message.author.id)
+            return False
+
+        return True # Kelly may proceed now
+
+    def ayakaTalk(self, message):
+        prompt = f"You are Ayaka, Kelly's Assistant. Kelly is cute discord mod bot with lively attitude and sass. Ayasaka is gorgeous, flirting, sexy assistant. Generate small Response with emojis. If user ask for a task, inform that you have added task in Kelly's schedules. Chat with user, flirt or simp and drive user away as its your only chance to make a move if needed or stay unamused."
+        response = getResponse(f"{message.author.display_name}: {message.content}", prompt)
+        self.kelly.memory.addUserChat(message.content, response, message.author.id, reply_by="Ayaka")
+        await self.ayakasend(message.channel, self.akayaEmojify(response), message.author.id)
+        if command:
+            self.busy.addScedules(user_id=message.author.id, command_name=command[0], params=command[1], message_id=message.id, channel_id=message.channel.id, priority=1):
+
+    def ayasakaQueueTask(self, message, command):
+        prompt = f"You are Ayaka, Kelly's Assistant. Kelly is cute discord mod bot with lively attitude and sass. Ayasaka is gorgeous, flirting, sexy assistant. Generate small Response with emojis. Inform User that you have added task in Kelly's schedules. Chat with user, flirt or simp and drive user away as its your only chance to make a move if needed or stay unamused."
+        response = getResponse(f"{message.author.display_name}: {message.content}", prompt)
+        self.kelly.memory.addUserChat(message.content, response, message.author.id, reply_by="Ayaka")
+        await self.ayakasend(message.channel, self.akayaEmojify(response), message.author.id)
+        self.busy.addScedules(user_id=message.author.id, command_name=command[0], params=command[1], message_id=message.id, channel_id=message.channel.id, priority=1):
+            
+    def akayaEmojify(self, message):
+        return message
