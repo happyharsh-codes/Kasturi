736e768d1516ac67b65c73c483221f2b5b2863d9
Happy Harsh <81058137+happyharsh-codes@users.noreply.github.com>
2025-12-19 09:52:44 +0530

    Update kellybusy.py

--- src/kellycore/kellybusy.py
+++ src/kellycore/kellybusy.py
@@ -10,54 +10,34 @@ class KellyBusy:
         - Kelly eventually performs task immediately
     """
 
-    def __init__(self):
-        pass
+    def __init__(self, schedules):
+        self._schedules = schedules 
 
-    def isBusy(self, mood, uid, type):
+    def isBusy(self):
         """Return whether Kelly is busy. Based on Mood not on Schedule."""
-        #Never busy for friends
-        if uid in Database["friends"]:
-            return False 
-        if mood["lazy"] > 90 or mood["sleepy"] > 90:
-            #Server Owners 40% chance listen on even busy
-            if "Server owner" in type and randint(1,10) <= 4:
-                return False
-            #Server Moderators 20% chance listens even on busy
-            elif "Moderator" in type and randint (1,5) == 1:
-                return False
-            elif mood["lazy"] > 90:
-                return "lazy"
-            elif mood["sleepy"] > 90:
-                return "sleepy"
-        return False
-
-    # ----------------------------------------------------------------------
-    #   AVAILABILITY CHECK
-    # ----------------------------------------------------------------------
-
-    def isKellyFree(self, guild_id):
-        """
-        Returns True if Kelly has free slots on her schedule.
-        """
-        if str(guild_id) not in Memory["schedules"]:
+        if len(self._schedules) > 5:
             return True
-        if Memory["schedules"][str(guild_id)] and len(Memory["schedules"][str(guild_id)]) > 5:
-            return False
-        return True
+        return False
         
-    def getNextAvailableTime(self, guild_id):
-        """
-        Returns Kelly's next free time slot today.
-        Returns only when empty slot is available on Kelly's schedule.
-        Otherwise returns None
-        """
-        if not str(guild_id) in Memory["schedules"]:
-            return None
-        schedules = Memory["schedules"][str(guild_id)]
-        if schedules:
-            if len(schedules) > 5:
-                return None
-            last_time = max(entry["time"] for entry in schedules)
-            return last_time
-        return None
+    def getNextFreeTime(self):
+        """Returns next Kelly free of schedule time"""
+        if not self._schedules:
+            return 0
+        last_time = max(datetime.fromisoformat(entry) for entry in self._schedules)
+        return last_time
+
+    def addSchedules(self, user_id, command_name, params, message_id, channel_id, priority=1):
+        """Adds task on Kelly's schedules"""
+        self._schedules[due.isoformat()] = {
+            "uid": user_id,
+            "command": command_name,
+            "params": params,
+            "message": message_id,
+            "channel": channel_id,
+            "priority": priority
+        }
+
+    def getSchedules(self)
+        return self._schedules
+            
             
