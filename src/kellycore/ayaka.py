48226319ad9fb88388778567176b6dd997d8fc09
Happy Harsh <81058137+happyharsh-codes@users.noreply.github.com>
2025-12-19 10:18:29 +0530

    Update ayaka.py

--- src/kellycore/ayaka.py
+++ src/kellycore/ayaka.py
@@ -21,18 +21,19 @@ class Ayaka:
 
     def __init__(self, kelly):
         self.kelly = kelly
-        self.busy = KellyBusy()
+        self.busy = KellyBusy(kelly.memory._memory["schedules"])
     
     async def ayakasend(self, channel, content, uid):
-        async with channel.typing():
-            try:
-                webhook = await channel.create_webhook(name="Ayaka")
+        try:
+            webhook = await channel.create_webhook(name="Ayaka")
+            async with channel.typing():
                 if isinstance(content, Embed):
                     await webhook.send(content= f"<@{uid}> ", embed=content, username="Ayaka", avatar_url=f"https://raw.githubusercontent.com/happyharsh-codes/Kasturi/refs/heads/main/assets/ayaka_{randint(1,3)}.png")
                 else:
                     await webhook.send(content= f"<@{uid}> " + content, username="Ayaka", avatar_url=f"https://raw.githubusercontent.com/happyharsh-codes/Kasturi/refs/heads/main/assets/ayaka_{randint(1,3)}.png")
-                await webhook.delete()
-            except:
+            await webhook.delete()
+        except:
+            async with channel.typing():
                 if isinstance(content, Embed):
                     await channel.send(f"**Ayaka**: <@{uid}> ", embed=content)
                 else:
@@ -107,18 +108,19 @@ class Ayaka:
             2. Manage schedule
         """
         uid = message.author.id
-
-        # If Kelly is too busy/sleepy/laz/tiredy, assistant manages
+       
+        # If Kelly is lazy, assistant manages
         state = self.busy.isBusy(mood, message.author.id, type)
-        if state:
-            prompt = f"You are Ayaka, Kelly's Assistant\nKelly is currently very {state}\nGenerate: Your Response in 20 words with emojis"
+        if self.kelly.status == "lazy":
+            prompt = f"You are Ayaka, Kelly's Assistant. Kelly is cute discord mod bot with lively attitude and sass. Kelly is currently very lazy, grumpy. Generate small Response with emojis. Inform user about Kelly's state. If user ask for a task, inform that you have added task in Kelly's schedules. Chat with user, flirt or simp and drive user away as its your only chance to make a move if needed or stay unamused. If its your first chat tell them they can chat with you by typing your name 'Ayasaka'."
             response = getResponse(f"{message.author.display_name}: {message.content}", prompt)
+            self.kelly.memory.addUserChat(message.content, response, message.author.id, reply_by="Ayaka")
             await self.ayakasend(message.channel, self.akayaEmojify(response), message.author.id)
             return False
         
-        # If schedule is overloaded -> Clear Decline
-        if not self.busy.isKellyFree(message.guild.id):
-            prompt = f"You are Akaya, Kelly's Assistant\nKelly's schedules are overloaded already, clearly decline user request.\nGenerate: Your Response in 20 words with emojis"
+        # If schedule is overloaded assistant handles
+        if self.kelly.status == "busy":
+            prompt = f"You are Akaya, Kelly's Assistant. Kelly is cute discord mod bot with lively attitude and saas. Kelly's schedules are overloaded already and shes very busy. Generate small Response with emojis. Inform user about Kelly's state. Inform that tasks cant be added as Kelly's schedules are full if user sends a task then only. Chat with user, flirt or simp and drive user away as its your only chance to make a move if needed. If its your first chat tell them they can chat with you by typing your name 'Ayasaka'"
             response = getResponse(f"{message.author.display_name}: {message.content}", prompt)
             await self.ayakasend(message.channel, self.akayaEmojify(response), message.author.id)
             return False
