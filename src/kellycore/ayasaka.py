7c2ca87d0e9b3b493a4bd87fefa54fb067aea84f
Happy Harsh <81058137+happyharsh-codes@users.noreply.github.com>
2025-12-20 15:17:01 +0530

    Update ayasaka.py

--- src/kellycore/ayasaka.py
+++ src/kellycore/ayasaka.py
@@ -19,7 +19,9 @@ class Ayasaka:
     def __init__(self, kelly):
         self.kelly = kelly
         self.busy = KellyBusy(kelly, kelly.memory._memory["schedules"])
-    
+        self._ayasaka = load_mongo_dict("Ayasaka", "kellymemory")
+        self._ayasaka.setdefault("new_user", [])
+        
     async def ayasakasend(self, channel, content, uid):
         try:
             webhook = await channel.create_webhook(name="Ayaka")
@@ -47,7 +49,26 @@ class Ayasaka:
             2. Manage schedule
         """
         uid = message.author.id
-       
+
+        #New User Initialisation 
+        if message.author.id not in self._ayasaka["new_user"]:
+            prompt = f"You are Ayasaka, Kelly's Assistant - cute, sexy and gorgeous girl.\nGenerate: Your Response in 20 words with 2-3 emoji. Generate a Initializing message for new user. name : {message.author.name} id: {message.author.id}"
+            response = getResponse(message.content, prompt, assistant=self.kelly.memory.getUserChats(message.author.id))
+            await self.ayasakasend(message.channel, response, message.author.id)
+            em = Embed(title= f"Hi I'm Ayasaka ", description="Hi I'm Ayasaka, Kelly's Personal Assistant. Kelly's schedules is my responsibility as you know Kelly gets busy very quickly with server stuff. So yeah I'm there for you when she's busy, I'll schedule your tasks, just call me. Alright so keep chatting in servers with Kelly. Respect boundaries and follow chat policy.", color = Color.green())
+            em.set_thumbnail(url= f"https://raw.githubusercontent.com/happyharsh-codes/Kasturi/refs/heads/main/assets/ayasaka_{randint(1,3)}.png")
+            dm_channel = message.author.dm_channel
+            self.kelly.memory.addUserChat(message.content, response, message.author.id, reply_by="Ayasaka")
+            if not dm_channel:
+                dm_channel = await message.author.create_dm()
+            try:
+                await dm_channel.send(embed = em)
+            except:
+                await self.ayasakasend(message.channel, em, message.author.id)
+            self.kelly.memory.modifyUserRelation(message.author.id, 2)
+            self._ayasaka["new_user"].append(message.author.id)
+            return True
+            
         # If Kelly is lazy, assistant manages
         if self.kelly.status == "lazy":
             prompt = f"You are Ayasaka, Kelly's Assistant. Kelly is cute discord mod bot with lively attitude and sass. Kelly is currently very lazy, grumpy. Generate small Response with emojis. Inform user about Kelly's state. If user ask for a task, inform that you have added task in Kelly's schedules. Chat with user, flirt or simp and drive user away as its your only chance to make a move if needed or stay unamused. If its your first chat tell them they can chat with you by typing your name 'Ayasaka'."
@@ -70,6 +91,18 @@ class Ayasaka:
         return True # Kelly may proceed now
 
     async def ayasakaTalk(self, message):
+       #New User Initialisation 
+        if message.author.id not in self._ayasaka["new_user"]:
+            em = Embed(title= f"Hi I'm Ayasaka ", description="Hi I'm Ayasaka, Kelly's Personal Assistant. Kelly's schedules is my responsibility as you know Kelly gets busy very quickly with server stuff. So yeah I'm there for you when she's busy, I'll schedule your tasks, just call me. Alright so keep chatting in servers with Kelly. Respect boundaries and follow chat policy.", color = Color.green())
+            em.set_thumbnail(url= f"https://raw.githubusercontent.com/happyharsh-codes/Kasturi/refs/heads/main/assets/ayasaka_{randint(1,3)}.png")
+            dm_channel = message.author.dm_channel
+            if not dm_channel:
+                dm_channel = await message.author.create_dm()
+            try:
+                await dm_channel.send(embed = em)
+            except:
+                await self.ayasakasend(message.channel, em, message.author.id)
+            self._ayasaka["new_user"].append(message.author.id)
         prompt = f"You are Ayasaka, Kelly's Assistant. Kelly is cute discord mod bot with lively attitude and sass. Ayasaka is gorgeous, flirting, sexy assistant. Generate small Response with emojis. If user ask for a task, inform that you have added task in Kelly's schedules. Chat with user, flirt or simp and drive user away as its your only chance to make a move if needed or stay unamused."
         response = getResponse(f"{message.author.display_name}: {message.content}", prompt, assistant=self.kelly.memory.getUserChats(message.author.id))
         self.kelly.memory.addUserChat(message.content, response, message.author.id, reply_by="Ayasaka")
