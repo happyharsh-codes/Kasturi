de962e744faa478543c4a1ef964ea4359077ad0b
Happy Harsh <81058137+happyharsh-codes@users.noreply.github.com>
2026-02-15 16:19:54 +0530

    Update games.py

--- src/cogs/games.py
+++ src/cogs/games.py
@@ -65,13 +65,17 @@ class Games(commands.Cog):
         profile = GameProfile(ctx.author.id)
         page = 0
         em = action_embed(f"{ctx.author.display_name}'s Inventory","",Color.green(),f"Inv by {ctx.author.name}",ctx.author.avatar)
+        
         #buttons
         left_btn = Button(emoji="<:leftarrow:1427527800533024839>", custom_id="left", style=ButtonStyle.blurple, disabled=True)
-        expand_btn = Button(label="Work", custom_id="expand", style=ButtonStyle.blurple)
-        right_btn = Button(label="<:rightarrow:1427527709403119646>", custom_id="right", style=ButtonStyle.blurple)
+        expand_btn = Button(label="Show", custom_id="expand", style=ButtonStyle.blurple)
+        right_btn = Button(emoji="<:rightarrow:1427527709403119646>", custom_id="right", style=ButtonStyle.blurple)
         
         def update(selected_category):
-            nonlocal em, profile
+            nonlocal em, profile, left_btn, right_btn, page
+            left_btn.disabled = page == 0
+            right_btn.disabled = page == len(categories)-1
+            
             descrip = f"**{selected_category.title()}**\n"
             items = profile.get(selected_category, {})
     
@@ -96,8 +100,6 @@ class Games(commands.Cog):
             nonlocal left_btn, expand_btn, right_btn, update, em, view, categories, page
             if inter.user.id != ctx.author.id:
                 return await inter.response.send_message(embed=Embed(description="This interaction is not for you", color=Color.red()),ephemeral=True)
-            left_btn.disabled = page == 0
-            right_btn.disabled = page == len(categories)-1
             
             if inter.data["custom_id"] == "left":
                 page -= 1
