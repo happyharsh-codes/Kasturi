14ae2c753a8c1a77d385341b86c6bb6800a399ba
Happy Harsh <81058137+happyharsh-codes@users.noreply.github.com>
2026-03-20 15:41:32 +0530

    Update ayasaka.py

--- src/kellycore/ayasaka.py
+++ src/kellycore/ayasaka.py
@@ -145,7 +145,7 @@ class Ayasaka:
         response = getResponse(f"{message.author.display_name}: {message.content}", prompt, assistant=self.kelly.memory.getUserChats(message.author.id))
         self.kelly.memory.addUserChat(message.content, response, message.author.id, reply_by="Ayasaka")
         await self.ayasakasend(message.channel, self.ayasakaEmojify(response), message.author.id)
-        self.busy.addSchedules(user_id=message.author.id, command_name=command, params=params, message_id=message.id, channel_id=message.channel.id, priority=1)
+        self.busy.addSchedules(command=command, params=params, channel=message.channel.id)
             
     def ayasakaEmojify(self, message):
         return message
