773d00085982ed7bddd5cfdc27bf24ad4985d014
Happy Harsh <81058137+happyharsh-codes@users.noreply.github.com>
2026-03-31 12:41:41 +0530

    Add await for player.queue.put_wait in music handler
    Fix missing await for player.queue.put_wait to ensure proper asynchronous behavior.

--- src/cogs/music&media.py
+++ src/cogs/music&media.py
@@ -219,7 +219,7 @@ class Music_and_Media(commands.Cog):
         seconds = duration % 60
         player: wavelink.Player = ctx.voice_client
         if not player:
-            player.queue.put_wait(track)
+            await player.queue.put_wait(track)
             player = await ctx.author.voice.channel.connect(cls=wavelink.Player)
         if player.playing and player.channel.id != channel.id:  
             return await ctx.send(embed= Embed(title=f"Cannot join your channel because currently playing in {player.channel.mention}", color = Color.red()))  
